34c863a62c07e1261e30f3ad7b7a374fa7f8afa9
jabir311 <jabir4771626@gmail.com>
2025-08-26 15:10:24 +0530

    Add files via upload

---
+++ if_else.java
@@ -0,0 +1,136 @@
+package Task;
+
+public class if_else {
+
+	public static void main(String[] args) {
+		
+		
+		//checking positive or negative number only using if statement
+//          int num=-1;
+//          if(num>0) {
+//        	  System.out.println("this is a positive number "+ num);
+//          }
+//          if(num<0) {
+//        	  System.out.println("this is a negative number "+ num);
+//          }
+		
+//		checking leapyear using if statement only
+//		 int year=2011;
+//		 if(year%4==0) {
+//			 System.out.println("this is a leapyear "+ year);
+//		 }
+//		 if(year%4!=0) {
+//			 System.out.println("this is not a leapyear "+ year);
+//
+//		 }
+		
+//		checking even or odd number using if statement only
+//		int num=11;
+//		if(num%2==0) {
+//			System.out.println("this is a even number "+ num);
+//		}
+//		if(num%2!=0) {
+//			System.out.println("this is a odd number "+ num);
+//
+//		}
+		
+//		checking prime or not using if only
+//		int num=8;
+//		int count=0;
+//		for(int i=1; i<=num; i++) {
+//			if(num%i==0) {
+//				count=count+1;
+//			}
+//		}
+//		if(count==2) {
+//			System.out.print("this is a prime number "+ count);
+//		}
+//		if(count!=2) {
+//			System.out.print("this is not aprime number "+ count);
+//		}
+		
+		
+//		}
+		
+//		printing reverse 10 to 1 only if using
+//		int i=10;
+//		if(i==10) {
+//			System.out.println(i);
+//			i=9;
+//		}
+//		if(i==9) {
+//			System.out.println(i);
+//			i=8;
+//		}
+//		if(i==8) {
+//			System.out.println(i);
+//			i=7;
+//		}
+//		if(i==7) {
+//			System.out.println(i);
+//			i=6;
+//		}
+//		if(i==6) {
+//			System.out.println(i);
+//			i=5;
+//		}
+//		if(i==5) {
+//			System.out.println(i);
+//			i=4;
+//		}
+//		if(i==4) {
+//			System.out.println(i);
+//			i=3;
+//		}
+//		if(i==3) {
+//			System.out.println(i);
+//			i=2;
+//		}
+//		if(i==2) {
+//			System.out.println(i);
+//			i=1;
+//		}
+//		if(i==1) {
+//			System.out.println(i);
+//		}
+	
+		//checking positive or negative number  using if else statement
+//		int num =-1;
+//		if(num>0) {
+//			System.out.println("positive number "+num);
+//		}else {
+//			System.out.println("negative number "+num);
+//
+//		}
+		
+//		checking leapyear using if else statement 
+//		int year=1996;
+//		if(year%4==0) {
+//			System.out.println("leap year "+year);
+//		}else {
+//			System.out.print("not leap year "+ year);
+//		}
+		
+//		checking even or odd number using if else statement only
+//		int num=10;
+//		if(num%2==0) {
+//			System.out.println("even number "+num);
+//		}else {
+//			System.out.println("odd number" + num);
+//		}
+		
+//		int prime=4;
+//		int count=0;
+//		for(int i=1; i<=prime; i++) {
+//			if(prime%i==0) {
+//				count= count+1;	
+//			}
+//		}
+//		if(count==2) {
+//			System.out.println("this is a prime number"+count);
+//		}else {
+//			System.out.println("this is not a prime number"+count);
+//		}
+		
+	}
+}
